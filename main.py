e81039b82546004e8653fb177f7d1fcb787f8516
AlexDickens <138562704+AlexDickens@users.noreply.github.com>
2023-07-07 00:14:38 +0100

    updates
    added tkinter and theme, copy to clipboard func and windows notifications

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import pyshorteners
+from tkinter import * 
+import tkinter as tk
+import pyperclip
+from win10toast import ToastNotifier
+import ttkbootstrap as tk
 
-urls = pyshorteners.Shortener()
+def urlShorten():
+    urls = pyshorteners.Shortener()
+    url = entry.get()
+    newURl = urls.tinyurl.short(url)
+    pyperclip.copy(newURl)
 
-print("Enter URL to shorten")
-url = input()
+    noti = ToastNotifier()
+    noti.show_toast("Congrats", "Your shortened URL has been copied to clipboard")
+
+    
+
+root = tk.Window(themename = "journal")
+root.geometry("240x100")
+root.title = "URL Shortener"
+entry = tk.Entry(root)
+entry.pack(side = "left", padx = 10)
+button = tk.Button(root, text = "Shorten", command = urlShorten)
+button.pack(side = "left", padx = 10)
 
 
-newURl = urls.tinyurl.short(url)
-print(newURl)
+
+root.mainloop()
+
